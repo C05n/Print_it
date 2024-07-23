3bdb2645e28c74ec98b37a582d97cc072e838f56
C05n <Erh.cedric@gmail.com>
2024-07-23 07:20:10 +0200

    Réorganisation

--- assets/script.js
+++ assets/script.js
@@ -17,37 +17,48 @@ const slides = [
 	}
 ];
 
-let currentSlide = 0;
+/*** Déclaration des différents éléments ***/ 
 const bannerImg = document.querySelector('.banner-img');
 const bannerText = document.querySelector('#banner p');
 const dots = document.querySelector('.dots');
 const arrowLeft = document.querySelector('.arrow_left');
 const arrowRight = document.querySelector('.arrow_right');
 
+/*** Mise de place de l'état de la slide ***/
+let currentSlide = 0;
+
+/*** Fonction de mise à jour des slides et des points ***/ 
+
 function updateSlide() {
-    bannerImg.src = "./assets/images/slideshow/" + slides[currentSlide].image;
-    bannerText.innerHTML = slides[currentSlide].tagLine;
-    updateDots();
+   bannerImg.src = "./assets/images/slideshow/" + slides[currentSlide].image;
+   bannerText.innerHTML = slides[currentSlide].tagLine;
+   updateDots();
 }
 
 function updateDots() {
-    dots.innerHTML = '';
-    for (let i = 0; i < slides.length; i++) {
-        const dot = document.createElement('div');
+   dots.innerHTML = '';
+   for (let i = 0; i < slides.length; i++) {
+       const dot = document.createElement('div');
         dot.classList.add('dot');
         if (i === currentSlide) dot.classList.add('dot_selected');
         dots.appendChild(dot);
-    }
+		  dot.addEventListener('click', () => {
+			   currentSlide = i;
+			   updateSlide();
+		  });
+   }
 }
 
+/*** Fonctionnalité des flèches ***/
+
 arrowLeft.addEventListener('click', () => {
-    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-    updateSlide();
+   currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+   updateSlide();
 });
 
 arrowRight.addEventListener('click', () => {
-    currentSlide = (currentSlide + 1) % slides.length;
-    updateSlide();
+   currentSlide = (currentSlide + 1) % slides.length;
+   updateSlide();
 });
 
 updateSlide();
